3b15343ec7bcefa9ca2107a43c05864654fff426
Sujit Baniya <spbaniya@deerwalk.com>
2016-01-28 16:54:14 +0545

    first update

---
+++ src/TestLibrary/SayTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spbaniya
+ * Date: 1/28/16
+ * Time: 4:41 PM
+ */
+
+namespace TestLibrary;
+
+
+class SayTest
+{
+    public static function test()
+    {
+        return "Hello World, This is test for composer!";
+    }
+}
